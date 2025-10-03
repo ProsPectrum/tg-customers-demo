c91c281c3edd959732d4b90e92fdbca7e5533ad6
Peter <pjotr.kravcenko@gmail.com>
2025-10-03 14:51:55 +0200

    added Header and Button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { useEffect } from 'react'
-const tg = window.Telegram.WebApp;
 
 function App() {
 
@@ -8,14 +7,9 @@ function App() {
     tg.ready()
   }, [])
 
-  const onClose = () => {
-    tg.close()
-  }
-
   return (
     <>
       work
-      <button onClick={onClose}>Aizvērt</button>
     </>
   )
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Button.css'
+
+const Button = ({ children, onClick, className }) => {
+  return <button className={"button"+className} onClick={onClick}>{children}</button>
+}
+
+export default Button
