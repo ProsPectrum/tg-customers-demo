2a15dbf823cf6c466aa1de6ac4056c2049afa856
Peter <pjotr.kravcenko@gmail.com>
2025-10-07 12:04:16 +0200

    Form modified

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Form.css'
 import { useState } from 'react';
 import { useTelegram } from '../../hooks/useTelegram';
 import '../Button/Button.css';
 
 const Form = () => {
+  const { tg, onClose } = useTelegram();
+
   const [deliveryTime, setDeliveryTime] = useState('1');
   const [paymentType, setPaymentType] = useState('1');
   const [deliveryAddress, setDeliveryAddress] = useState('');
   const [additionalInformation, setAdditionalInformation] = useState('');
+
+  useEffect(() => {
+    tg.MainButton.setText("Sūtīt");
+    tg.MainButton.onClick(() => {
+        onClose();
+    });
+  }, [])
 
   const onChangeDeliveryTime = (e) => {
     setDeliveryTime(e.target.value);
@@ -29,7 +38,7 @@ const Form = () => {
   return (
     <div className="form">
         <h3>Ievadiet savus datus</h3>
-        <select className="select" value={deliveryTime} onChange={onChangeDeliveryTime}>
+        <select className="select" value={paymentType} onChange={onChangePaymentType}>
             <option value="1" disabled>Maksājuma veids</option>
             <option value="2">Maksājums skaidrā tikšanās brīdī</option>
             <option value="3">Apmaksa ar karti tikšanās brīdī</option>
@@ -44,7 +53,6 @@ const Form = () => {
             <option value="5">17:00-20:00</option>
         </select>
         <input type="text" placeholder="Papildu informācija/lūgumi" className="input" value={additionalInformation} onChange={onChangeAdditionalInformation} />
-        <button className="button">Sūtīt</button>
     </div>
   )
 }
